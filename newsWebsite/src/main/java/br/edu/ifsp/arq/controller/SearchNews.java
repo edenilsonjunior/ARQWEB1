0b4b80750f35a05a447a9c56d057677e16a7ac12
roberts <gabrielrobertssoares@gmail.com>
2024-08-10 03:18:26 -0300

    feature: page search news default and servlet

--- newsWebsite/src/main/java/br/edu/ifsp/arq/controller/SearchNews.java
+++ newsWebsite/src/main/java/br/edu/ifsp/arq/controller/SearchNews.java
@@ -1,10 +1,11 @@
 package br.edu.ifsp.arq.controller;
 
-//import br.edu.ifsp.arq.model.dao.NewsArticleDAO;
+import br.edu.ifsp.arq.model.dao.MockNewsArticleDAO;
 import br.edu.ifsp.arq.model.entity.NewsArticle;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,34 +13,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SearchNews
- */
 @WebServlet("/search")
 public class SearchNews extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public SearchNews() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String keyword = request.getParameter("keyword");
-//
-////        List<NewsArticle> newsList = NewsArticleDAO.getInstance().searchNews(keyword);
-//
-//        request.setAttribute("news_list", newsList);
-//        RequestDispatcher dispatcher = request.getRequestDispatcher("news.jsp");
-//        dispatcher.forward(request,response);
+        String keyword = request.getParameter("keyword");
+        List<NewsArticle> newsList = MockNewsArticleDAO.getInstance().getNewsArticleSearched(keyword);
+        List<NewsArticle> filteredNewsList = newsList.stream()
+            .filter(newsArticle -> newsArticle.getSummary().contains(keyword) ||
+               newsArticle.getText().contains(keyword) ||
+               newsArticle.getTitle().contains(keyword) ||
+               newsArticle.getAuthor().contains(keyword) ||
+               newsArticle.getSource().contains(keyword))
+               .collect(Collectors.toList());
+
+        request.setAttribute("listNews", filteredNewsList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("newsSearch.jsp");
+        dispatcher.forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 }
--- newsWebsite/src/main/webapp/newsSearch.jsp
+++ newsWebsite/src/main/webapp/newsSearch.jsp
@@ -4,39 +4,26 @@
 <%@ include file="includes/navbar-logged-in.jsp" %>
 <%@ taglib  uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
-<%--Codigo para pegar dados das noticias--%>
-<%--<main class="container">--%>
-<%--    <div class="list-group">--%>
-<%--        <c:if test="${not empty listNews}">--%>
-<%--            <c:set var="listItemClass" value="list-group-item list-group-item-action active" style="display: block; width: 50%; margin: 0 auto;"/>--%>
-<%--                <c:forEach var="news" items="${listNews}">--%>
-<%--                    <a href="#" class="${listItemClass}" aria-current="true">--%>
-<%--                        <div class="d-flex w-100 justify-content-between">--%>
-<%--                            <h5 class="mb-1">${news.title}</h5>--%>
-<%--                            <p class="mb-1">${news.text}</p>--%>
-<%--                            <small>${news.author}</small>--%>
-<%--                        </div>--%>
-<%--                    </a>--%>
-<%--                </c:forEach>--%>
-<%--        </c:if>--%>
-<%--    </div>--%>
-<%--</main>--%>
-
 <h1 align="center">Noticias</h1>
 
 <main class="container">
-    <div class="bk-book p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-        <c:if test="${not empty listNews}">
-            <c:forEach var="news" items="${listNews}">
-                <div class="col-lg-6 px-0 ">
+    <c:if test="${not empty listNews}">
+        <c:forEach var="news" items="${listNews}">
+            <div class="bk-book p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-tertiary">
+                <div class="col-lg-6 px-0">
                     <h1 class="display-4 fst-italic">${news.title}</h1>
                     <p class="lead my-3">${news.text}</p>
-                    <p class="lead mb-0"><a href="news.jsp" class="fw-bold">Continue reading...</a></p>
+                    <p class="lead mb-0"><a href="news?id=${news.id}" class="fw-bold">Continue reading...</a></p>
                     <small>${news.author}</small>
                 </div>
-            </c:forEach>
-        </c:if>
-    </div>
+            </div>
+        </c:forEach>
+    </c:if>
+    <c:if test="${empty listNews}">
+        <div class="alert alert-warning" role="alert">
+            Nenhuma notícia encontrada para a busca realizada.
+        </div>
+    </c:if>
 </main>
 
 <%@ include file="includes/footer.jsp" %>
