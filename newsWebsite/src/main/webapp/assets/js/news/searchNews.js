f311301aac2ca179d5ef45dc0d71e37df6cfa088
Edenilson Junior <edenilsonju10@gmail.com>
2024-09-09 16:54:12 -0300

    ajustes na barra de busca

--- newsWebsite/src/main/webapp/assets/js/news/searchNews.js
+++ newsWebsite/src/main/webapp/assets/js/news/searchNews.js
@@ -3,9 +3,12 @@ import {contextPath} from '../components/global.js';
 document.addEventListener("DOMContentLoaded", async function () {
     const params = new URLSearchParams(window.location.search);
     const keyword = params.get('keyword');
+
     if (keyword) {
-        const newsList = await fetchNewsByKeyword(keyword);
-        await loadSearchNews(newsList);
+        const response = await fetchNewsByKeyword(encodeURIComponent(keyword));
+        await loadSearchNews(response.newsList);
+    } else {
+        displayNoResults('Por favor, insira uma palavra-chave para realizar a busca.');
     }
 });
 
@@ -19,28 +22,40 @@ const fetchNewsByKeyword = async (keyword) => {
     }
 }
 
-const loadSearchNews = async (newsList) =>{
+const loadSearchNews = async (newsList) => {
+    const newsContainer = document.getElementById('news');
 
-    const newsContainer = document.getElementById('newsSearched');
-    let content = `<div class="col-lg-6 px-0">`;
+    if (newsList.length === 0) {
+        newsContainer.innerHTML = `
+            <div class="alert alert-warning" role="alert">
+                Nenhuma notícia encontrada para a busca realizada.
+            </div>
+        `;
+        return;
+    }
 
-    if(newsList.length === 0){
+
+    let content = '';
+    newsList.forEach((news) => {
         content += `
             <div class="p-4 p-md-5 mb-4 rounded border text-body-emphasis bg-body-tertiary">
-                <div class="alert alert-warning" role="alert">
-                    Nenhuma notícia encontrada para a busca realizada.
+                <div class="col-lg-6 px-0">
+                    <h1 class="display-4 fst-italic">${news.title}</h1>
+                    <p class="lead my-3">${news.summary}</p>
+                    <p class="lead my-3">Autor: ${news.author}</p>
+                    <p class="lead mb-0"><a href="${contextPath}views/news/news.html?id=${news.id}" class="fw-bold">Continue lendo...</a></p>
                 </div>
             </div>
         `;
-    } else {
-        content += `
-            <div class="col-lg-6 px-0">
-                <h1 class="display-4 fst-italic">${news.title}</h1>
-                <p class="lead my-3">${news.summary}</p>
-                <p class="lead my-3">Autor: ${news.author}</p>
-                <p class="lead mb-0"><a href="${contextPath}/views/news/news.html?id=${news.id}" class="fw-bold">Continue lendo...</a></p>
-            </div>
-        `;
-    }
+    });
     newsContainer.innerHTML = content;
+}
+
+const displayNoResults = (message) => {
+    const newsContainer = document.getElementById('news');
+    newsContainer.innerHTML = `
+        <div class="alert alert-warning" role="alert">
+            ${message}
+        </div>
+    `;
 }
