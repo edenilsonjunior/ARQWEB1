fe68cad676896eba3632819a89088a52c12ad6dc
Edenilson Junior <edenilsonju10@gmail.com>
2024-09-09 13:57:41 -0300

    ajustes na navbar para possuir um template

--- newsWebsite/src/main/webapp/assets/js/components/navbar.js
+++ newsWebsite/src/main/webapp/assets/js/components/navbar.js
@@ -2,87 +2,67 @@ import {checkLoginStatus, contextPath} from './global.js';
 
 const navbarContainer = document.getElementById('nav-bar');
 
-
 document.addEventListener('DOMContentLoaded', async () => {
 
+    const response = await fetch(`${contextPath}/assets/js/components/navbar-template.html`);
+    navbarContainer.innerHTML = await response.text();
+
     let data = await checkLoginStatus();
-    navbar(data.isLogged);
+    await customizeNavbar(data.isLogged);
 });
 
-const navbar = (isLogged)=> {
+const customizeNavbar = async (isLogged) => {
 
-    let content = `
-        <div class="container">
-            <header class="border-bottom lh-1 py-2">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#"><img alt="logo" src="${contextPath}/assets/images/IFNews.png" width="100px"></a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="${contextPath}/index.html">Página Inicial</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="${contextPath}/retrieve-category">Categorias</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="${contextPath}/about.html">Sobre</a>
-                                </li>
-    `;
+    document.getElementById('nav-logo').src = `${contextPath}/assets/images/IFNews.png`;
+    loadNavbarLinks(isLogged);
 
-    if (isLogged) {
-        content += `
-           <li class="nav-item">
+    document.getElementById('search-form').action = `${contextPath}/views/news/newsSearch.html`;
+
+    loadAuthLinks(isLogged);
+};
+
+const loadNavbarLinks = (isLogged) =>{
+
+    document.getElementById('nav-links').innerHTML = `
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="${contextPath}/index.html">Página Inicial</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="${contextPath}/retrieve-category">Categorias</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="${contextPath}/about.html">Sobre</a>
+        </li>
+        ${isLogged ? `
+            <li class="nav-item">
                 <a class="nav-link" href="${contextPath}/views/news/createNewsArticle.html">Cadastrar Noticia</a>
+            </li>
+        ` : ''}
+    `;
+}
+
+const loadAuthLinks = (isLogged)  =>{
+    const authLinks = document.getElementById('auth-links');
+    if (!isLogged) {
+        authLinks.innerHTML = `
+            <li class="nav-item">
+                <a href="${contextPath}/signup" class="btn btn-outline-secondary" role="button">SignUp</a>
+                <a href="${contextPath}/login" class="btn btn-secondary" role="button">Login</a>
+            </li>
+        `;
+    } else {
+        authLinks.innerHTML = `
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt="user" width="30px">
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="${contextPath}/views/user/profile.html">Perfil</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="${contextPath}/logout">Sair</a></li>
+                </ul>
             </li>
         `;
     }
 
-    content += '</ul>';
-
-    content += `
-                <form class="d-flex" role="search" action="${contextPath}/views/news/newsSearch.html" method="get">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="keyword">
-                <button class="btn btn-light" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-              </form>`;
-
-    if (!isLogged) {
-
-        content += `
-            <ul class="navbar-nav me-right mb-2 mb-lg-0 mx-1 mt-2">
-                <li class="nav-item dropdown">
-                    <a href="${contextPath}/signup" class="btn btn-outline-secondary" role="button">SignUp</a>
-                    <a href="${contextPath}/login" class="btn btn-secondary" role="button">Login</a>
-                </li>
-            </ul>
-        `;
-    } else {
-        content += `
-            <ul class="navbar-nav me-right mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" alt="user" width="30px">
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="${contextPath}/views/user/profile.html">Perfil</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="${contextPath}/logout">Sair</a></li>
-                    </ul>
-                </li>
-            </ul>        
-        `;
-    }
-
-    content += `
-            </div>
-          </div>
-        </nav>
-      </header>`;
-
-    navbarContainer.innerHTML = content;
 }
